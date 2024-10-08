492061e2b7999c7a8b7b246c42616966f020b1ac
Ravan Alaskarov <ravanalaskarov@proton.me>
2024-10-08 23:54:16 +0400

    array resizer

---
+++ ArrayResizer/ArrayResizer/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] givenArray = [1, 2, 3, 4, 5];
+
+int newSize = 6;
+
+int[] newArray = new int[newSize];
+
+int defaultDigit = default;
+
+for (int i = 0; i < newSize; i++)
+{
+    if (i < givenArray.Length)
+    {
+        defaultDigit = givenArray[i];
+    }
+    else
+    {
+        defaultDigit = default;
+    }
+    newArray[i] = defaultDigit;
+}
+
+foreach (int element in newArray)
+{
+    Console.Write(element);
+    Console.Write(" ");
+}

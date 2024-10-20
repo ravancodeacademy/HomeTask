9010484249ccf8134555349a6b3da80f2ecb0b35
Ravan Alaskarov <ravanalaskarov@proton.me>
2024-10-20 13:35:58 +0400

    weapon project

---
+++ WeaponProject/WeaponProject/Models/Weapon.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponProject.Models;
+
+internal class Weapon(int maxBulletCount, int currentBulletCount, WeaponFireMode fireMode)
+{
+    private const float BulletFireInSeconds = 0.5f;
+    private int _maxBulletCount = maxBulletCount;
+    private int _currentBulletCount = currentBulletCount;
+    private WeaponFireMode _fireMode = fireMode;
+
+    public bool Shoot()
+    {
+        if (_currentBulletCount == 0)
+        {
+            return false;
+        }
+        _currentBulletCount--;
+        return true;
+    }
+
+    public float Fire()
+    {
+        float secondsTookToFire = _currentBulletCount * BulletFireInSeconds;
+        _currentBulletCount = 0;
+        return secondsTookToFire;
+    }
+
+    public int GetNeededBulletCount()
+    {
+        return _maxBulletCount - _currentBulletCount;
+    }
+
+    public void Reload()
+    {
+        _currentBulletCount = _maxBulletCount;
+    }
+
+    public WeaponFireMode ChangeFireMode()
+    {
+        _fireMode = _fireMode == WeaponFireMode.single ? WeaponFireMode.auto : WeaponFireMode.single;
+        return _fireMode;
+    }
+
+
+    public string GetInfo()
+    {
+        return $"Max bullet count: {_maxBulletCount}\nCurrent bullet count:{_currentBulletCount}\nFire mode:{_fireMode}";
+    }
+}
---
+++ WeaponProject/WeaponProject/Program.cs
@@ -0,0 +1,70 @@
+﻿using WeaponProject.Models;
+
+namespace WeaponProject;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Weapon weapon = new(200, 50, WeaponFireMode.single);
+        Console.WriteLine("0 - Get info");
+        Console.WriteLine("1 - Shoot");
+        Console.WriteLine("2 - Fire");
+        Console.WriteLine("3 - Needed bullet count");
+        Console.WriteLine("4 - Reload");
+        Console.WriteLine("5 - Change fire mode");
+        Console.WriteLine("6 - Exit");
+
+        while (true)
+        {
+          
+            Console.Write("Enter your selection: ");
+            byte.TryParse(Console.ReadLine(), out var index);
+
+            if (index == 6)
+            {
+                Console.WriteLine("Exitting...");
+                break;
+            }
+
+
+            switch(index)
+            {
+                case 0:
+                    Console.WriteLine(weapon.GetInfo());
+                    Console.WriteLine();
+
+                    break;
+                case 1:
+                    if (!weapon.Shoot())
+                    {
+                        Console.WriteLine($"Couldn't shoot\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Shooted 1 bullet\n");
+                    }
+                    break;
+                case 2:
+                    float seconds = weapon.Fire();
+                    if (seconds == 0)
+                    {
+                        Console.WriteLine("No bullet\n");
+                    }
+                    Console.WriteLine($"It took {seconds} seconds\n");
+                    break;
+                case 3:
+                    Console.WriteLine(weapon.GetNeededBulletCount());
+                    Console.WriteLine();
+                    break;
+                case 4:
+                    weapon.Reload();
+                    Console.WriteLine("Reloaded\n");
+                    break;
+                case 5:
+                    WeaponFireMode fireMode =  weapon.ChangeFireMode();
+                    Console.WriteLine($"Firemode changed to {fireMode}\n");
+                    break;
+            }
+        }
+    }
+}
